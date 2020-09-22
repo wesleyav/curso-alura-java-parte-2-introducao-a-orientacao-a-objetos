3ad0fb7c757cbb35db053d40072d1768de1b6fdb
Wesley <wesley.contato@hotmail.com>
2020-09-21 22:19:15 -0300

    Valores default de atributos

--- bytebank/src/bytebank/CriaConta.java
+++ bytebank/src/bytebank/CriaConta.java
@@ -15,5 +15,15 @@ public class CriaConta {
 
 		System.out.println("primeira conta tem " + primeiraConta.saldo);
 		System.out.println("segunda conta tem " + segundaConta.saldo);
+		
+		System.out.println(primeiraConta.agencia);
+		System.out.println(primeiraConta.numero);
+		
+		System.out.println(segundaConta.agencia);
+		
+		segundaConta.agencia = 146;
+		
+		System.out.println("agora a sengunda conta está na agência " + segundaConta.agencia);
+		
 	}
 }
