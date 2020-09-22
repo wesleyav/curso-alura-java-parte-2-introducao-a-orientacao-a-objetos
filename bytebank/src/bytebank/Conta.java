7f2c721a1c759be36d10dc988a09144bb64cd979
Wesley <wesley.contato@hotmail.com>
2020-09-22 08:50:09 -0300

    Métodos com referência e mais retorno

--- bytebank/src/bytebank/Conta.java
+++ bytebank/src/bytebank/Conta.java
@@ -8,15 +8,24 @@ public class Conta {
 	String titular;
 
 	public void deposita(double valor) {
-		this.saldo = saldo + valor;
+		this.saldo += valor;
 	}
 
 	public boolean saca(double valor) {
 		if (this.saldo >= valor) {
-			this.saldo = this.saldo - valor;
+			this.saldo -= valor;
 			return true;
 		} else {
 			return false;
 		}
+	}
+	
+	public boolean transfere(double valor, Conta destino ){
+		if(this.saldo >= valor){
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		}
+			return false;
 	}
 }
--- bytebank/src/bytebank/TestaMetodo.java
+++ bytebank/src/bytebank/TestaMetodo.java
@@ -7,9 +7,21 @@ public class TestaMetodo {
 		contaDoPaulo.saldo = 100;
 		contaDoPaulo.deposita(50);
 		System.out.println(contaDoPaulo.saldo);
-		
+
 		boolean conseguiuRetirar = contaDoPaulo.saca(20);
 		System.out.println(contaDoPaulo.saldo);
 		System.out.println(conseguiuRetirar);
+
+		Conta contaDaMarcela = new Conta();
+		contaDaMarcela.deposita(1000);
+		boolean sucessoTransferencia = contaDaMarcela.transfere(3000, contaDoPaulo);
+
+		if (sucessoTransferencia) {
+			System.out.println("Transferência com sucesso");
+		} else {
+			System.out.println("Faltou dinheiro");
+		}
+		System.out.println(contaDaMarcela.saldo);
+		System.out.println(contaDoPaulo.saldo);
 	}
 }
